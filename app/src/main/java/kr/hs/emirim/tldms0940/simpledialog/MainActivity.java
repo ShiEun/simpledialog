c5d8223bd6a8f9675be34403cee273723f642a0d
Shieun Song <tldms0940@naver.com>
2016-06-07 20:50:24 +0900

    #4 array

--- app/src/main/java/kr/hs/emirim/tldms0940/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/tldms0940/simpledialog/MainActivity.java
@@ -1,13 +1,48 @@
 package kr.hs.emirim.tldms0940.simpledialog;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.DialogInterface;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    String[] items ={"젤리빈","킷켓","롤리팝"};
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but=(Button)findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("First Dialog");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                but.setText(items[which]);
+            }
+        });
+        dialog.setIcon(R.drawable.first_icon);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"대화상자의 확인 버튼을 클릭했음..",Toast.LENGTH_LONG).show();
+            }
+        });
+        dialog.show();
     }
 }
